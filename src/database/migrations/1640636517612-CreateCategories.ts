75f0d2ba8b457e74ac8d028db8c24afb22e54e68
RODRIGO SOUZA CAMELO <113525@pmenos.com.br>
2021-12-27 19:22:29 -0300

    first commit

---
+++ src/database/migrations/1640636517612-CreateCategories.ts
@@ -0,0 +1,39 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateCategories1640636517612 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'categories',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid'
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isUnique: true
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('categories');
+    }
+
+}
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,26 @@
+import { getRepository } from "typeorm";
+import { Category } from "../entities/Category";
+
+type CategoryRequest = {
+    name: string;
+    description: string;
+}
+
+export class CreateCategoryService {
+    async execute({ name, description }: CategoryRequest): Promise<Category | Error> {
+        const repository = getRepository(Category);
+
+        if(repository.findOne({ name })) {
+            return new Error("Category already exists")
+        }
+
+        const category = repository.create({
+            name,
+            description
+        });
+
+        await repository.save(category);
+
+        return category;
+    }
+}
